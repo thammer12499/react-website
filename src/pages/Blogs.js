7b3ed28f6504e361914c36cedf9c9b02173a6b2b
thammer12499 <thammer12499@gmail.com>
2023-12-04 17:00:47 -0500

    GHPages package addition

--- src/pages/Blogs.js
+++ src/pages/Blogs.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Blogs = () => {
-    return <h1>You can write your blogs!</h1>;
+    return <h1>You can write your blogs! Need this for my update for GH pages.</h1>;
 };
 
 export default Blogs;
