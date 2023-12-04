7f493bdd5e02a940a21dad0cb204c5751d8044d0
thammer12499 <thammer12499@gmail.com>
2023-12-04 17:21:08 -0500

    Small change to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Navbar from './components/Navbar';
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, } from "react-router-dom";
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
